ff8e81e22ebe0a51caa85d6b8b68b8251335c991
unknown <y377075@gmail.com>
2025-10-07 18:17:58 +0530

    Added the tab image

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -14,14 +14,20 @@ function NavBar() {
   console.log(showHamburgerMenu)
 
   return (
+    <div>
     <nav className="navbar">
+      {/* Logo */}
       <div className='logo'> 
         <i className="fa-solid fa-less-than"></i>
         <span >Yash <span className='lastName'>Singhal /</span></span>
         <i className="fa-solid fa-greater-than lastName"></i></div>
+
+        {/* Hamburger Open */}
          <div className="bar " onClick={toggleHamburgerMenu}>
         <i className="fa-solid fa-bars"></i>
       </div>
+      
+      {/* Lists Visible On Desktop */}
       <ul className="nav-ul">
         <li className="nav-li"><a href="#home">Home</a></li>
         <li className="nav-li"><a href="#about">About</a></li>
@@ -29,11 +35,16 @@ function NavBar() {
         <li className="nav-li"><a href="#skills">Skills</a></li>
         <li className="nav-li"><a href="#projects">Projects</a></li>
         <li className="nav-li"><a href="#contact">Contact</a></li>
-        {/* <li className='nav-li bar'><i class="fa-solid fa-bars"></i></li> */}
       </ul>
 
-            <div className={showHamburgerMenu  ? "hambuger-hide"  : "hambuger-show"}>
-              <span  className="ham-cut-sign" onClick={toggleHamburgerMenu}><i className="fa-solid fa-xmark close-menu"></i></span>
+
+     
+    </nav>
+
+
+{/* Hamburger Close */}
+            <div className={`hambuger-hide ${showHamburgerMenu && "hambuger-show"}`}>
+              <div  className="close-menu" onClick={toggleHamburgerMenu}><i className="fa-solid fa-xmark "></i></div>
     <ul className="ham-hide-nav-ul">
     
     <li className="nav-li-cut"><a href="#home">Home</a></li>
@@ -45,8 +56,16 @@ function NavBar() {
 
     </ul>
 </div>
-     
-    </nav>
+
+
+ <div >
+    <div className={`backdrop ${showHamburgerMenu && "backdrop-show"}`}  onClick={toggleHamburgerMenu}></div>
+
+
+ </div>
+
+
+    </div>
   );
 }
 
